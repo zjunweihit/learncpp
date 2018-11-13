d8814a3ed95b06266adde8e24a8acbd8509eb1df
Junwei Zhang <zjunweihit@sina.com>
2018-11-13 22:20:39 +0800

    basic: add cases for inheritance

--- basic/inheritance.cpp
+++ basic/inheritance.cpp
@@ -139,6 +139,7 @@ namespace Test3
     {
     protected:
         int m_value;
+        void proFun() { std::cout << "Base protected function\n"; }
 
     public:
         Base(int value)
@@ -147,13 +148,27 @@ namespace Test3
         }
 
         // only in base class
+        // Note: if it's defined as private func, it will cause compiling error
+        //       since, it cannot be accessed by Derived class, if Derived class
+        //       calls it.
         void identify() { std::cout << "The function in Base only\n"; }
 
         // will be overided by Derived class
         void print() { std::cout << "I am a printer in Base\n"; }
 
+        // will be overided by Derived class
+        void print2() { std::cout << "I am another printer in Base\n"; }
+
         // will be hided by Derived class
         void hide() { std::cout << "show in Base, hide in Derived\n"; }
+
+        // friend function for Base, but not belongs to Base
+        friend std::ostream& operator<< (std::ostream &out, const Base &b)
+        {
+            out << "In Base\n";
+            out << b.m_value << '\n';
+            return out;
+        }
     };
 
     class Derived: public Base
@@ -162,19 +177,45 @@ namespace Test3
         int m_cost;
 
     public:
+        // Note: we cannot initialize m_value using intialization list
+        // calling Base(value) or assigning value in constructor function
+        // Since it's Base member (Base must intialize it)
         Derived(int value)
             : Base(value), m_cost(2 * value)
         {
         }
 
         // overriding Base class
+        // Note: access specifier is decided by Derived class rather than Base class
         void print() { std::cout << "I am a printer in Derived\n"; }
+
+        // Another print function to call Base function as well.
+        // better to specify the namespace clearly.
+        // can we call print2() by Base implicitly? segment fault in MacOS
+        void print2()
+        {
+            Base::print2();
+            std::cout << "I am another printer in Derived\n";
+        }
 
         // a new function in Derived class
         void getValue() { std::cout << "created in Derived "<< m_cost << "\n"; }
 
         // a hided function cannot be called by Derived class
         void hide() = delete;
+
+        // friend function to call Base value
+        friend std::ostream& operator<< (std::ostream &out, const Derived &d)
+        {
+            out << "In Derived\n";
+            // static_cast Derived to a Base object, so we call the right version of operator<<
+            out << static_cast<Base>(d); 
+            return out;
+        }
+
+        // change protected function to public, called by others.
+        // Note: no parenthesis here.
+        using Base::proFun;
     };
 
     void run(void)
@@ -184,10 +225,13 @@ namespace Test3
         Base base(5);
         Derived derived(7);
 
-        derived.identify(); // exist in base class only
+        derived.identify(); // exist in base class only, it should be not private
         derived.print();    // overided by Derived class
+        derived.print2();   // overided by Derived class, calling Base print
         derived.getValue(); // a new function in Derived class
         //derived.hide();   // it's hided in Derived class, not compiling
+        std::cout << derived; // friend func to call Base value
+        derived.proFun();   // call a Base protected function
 
         std::cout << "\n=== Test 3: end ===\n\n";
     }
