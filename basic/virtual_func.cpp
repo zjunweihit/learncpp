58d7a794142f17838a986ba27f11d10fa9c66c46
Junwei Zhang <zjunweihit@sina.com>
2018-11-24 16:04:51 +0800

    basic: override, final, covariant specifier(C++11)

--- basic/virtual_func.cpp
+++ basic/virtual_func.cpp
@@ -112,7 +112,116 @@ namespace Test1
     }
 };
 
+/*
+ * === Test 2: override, final, covariant specifier(C++11) ===
+ * overide:
+ *   (1) use it as much as possible
+ *   (2) applied to any override function by placing the specifier in the
+ *       same place const would go.
+ *       If the function is not going to be a override func, compiling error
+ *   Note:
+ *       Apply the override specifier to every intended override function you write.
+ *
+ * final:
+ *   (1) don't use it so much in practice
+ *   (2) the function will be override in later derived functions
+ *
+ * Covariant return types
+ *   one special case in which a derived class virtual function override can
+ *   have a different return type than the base class and still be considered
+ *   a matching override.
+ *   If the return type of a virtual function is a pointer or a reference to
+ *   a class, override functions can return a pointer or a reference to a
+ *   derived class. This is called Covariant return type.
+ */
+namespace Test2
+{
+    class A
+    {
+    public:
+        virtual const char* getName1(int x) { return "A"; }
+        virtual const char* getName2(int x) { return "A"; }
+        virtual const char* getName3(void) { return "A"; }
+    };
+
+    class B : public A
+    {
+    public:
+        virtual const char* getName1(int x) override { return "B"; }
+
+        // comipling error, the function is not override the base function
+        //virtual const char* getName2(short x) override { return "B"; }
+
+        virtual const char* getName2(short x) { return "B"; }
+
+        // note use of final specifier on following line
+        // that makes this function no longer overridable
+        virtual const char* getName3(void) override final { return "B"; }
+    };
+
+    // C is final class, not use it after "public B"
+    class C final : public B
+    {
+    public:
+        // compile error: overrides B::getName(), which is final
+        //virtual const char* getName3(void) override { return "C"; }
+    };
+
+    // compiling error: C is the final class, cannot inherit from it.
+    //class D : public C
+    //{
+    //};
+
+    void vf_override_final(void)
+    {
+        B b;
+        A &rBase = b;
+        std::cout << "call base getName1(override): " << rBase.getName1(1) << '\n';
+        std::cout << "call base getName2(no override): " << rBase.getName2(2) << '\n';
+    }
+
+    class Base
+    {
+    public:
+        // This version of getThis() returns a pointer to a Base class
+        virtual Base* getThis() { std::cout << "called Base::getThis()\n"; return this; }
+        void printType() { std::cout << "returned a Base\n"; }
+    };
+
+    class Derived : public Base
+    {
+    public:
+        // Normally override functions have to return objects of the same type as the base function
+        // However, because Derived is derived from Base, it's okay to return Derived* instead of Base*
+        virtual Derived* getThis() { std::cout << "called Derived::getThis()\n";  return this; }
+        void printType() { std::cout << "returned a Derived\n"; }
+    };
+
+    // We call b->getThis(). Variable b is a Base pointer to a Derived object.
+    // Base::getThis() is virtual function, so this calls Derived::getThis().
+    // Although Derived::getThis() returns a Derived*, because base version of
+    // the function returns a Base*, the returned Derived* is downcast to a
+    // Base*. And thus, Base::printType() is called.
+    void vf_covariant(void)
+    {
+        Derived d;
+        Base *b = &d;
+        d.getThis()->printType(); // calls Derived::getThis(), returns a Derived*, calls Derived::printType
+        b->getThis()->printType(); // calls Derived::getThis(), returns a Base*, calls Base::printType
+    }
+
+    void fn(void)
+    {
+        std::cout << "\n <<< virtual function override and final >>>\n";
+        vf_override_final();
+
+        std::cout << "\n <<< virtual function covariant >>>\n";
+        vf_covariant();
+    }
+}
+
 int main()
 {
-    run(1, &(Test1::fn)); // template function and class
+    run(1, &(Test1::fn)); // virtual function basis
+    run(2, &(Test2::fn)); // override and final specifier
 }
